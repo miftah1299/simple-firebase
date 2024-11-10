d33ae01af7269360ca055aa35e6a31c4df929151
miftah1299 <jannat27120@gmail.com>
2024-11-10 16:21:07 +0600

    implement Google authentication with Firebase in Login component and update steps for configuration

--- src/__steps__.js
+++ src/__steps__.js
@@ -6,7 +6,7 @@ step 2: install firebase: npm install firebase
 step 3: console firebase
 step 4: create a firebase project
 step 5: create a web app in firebase
-step 6: copy the firebase config
+step 6: copy the firebase config (DANGER: DO NOT SHARE CONFIG PUBLICLY)
 step 7: create a file named firebase.init.js in src/firebase
 step 8: paste the firebase config in firebase.init.js
 
@@ -17,9 +17,11 @@ step 11: export auth from firebase.init.js
 step 12: import auth in src/components/Login.jsx
 
 step 13: in the console authentification > enable Google Provider
+step 14: import GoogleAuthProvider in src/components/Login.jsx
 
-
-
-
-
+step 15: Sign in with Google
+    1. Import the auth object from firebase/init.js
+    2. Call the signInWithPopup method on the auth object
+    3. Pass the provider object as an argument to the signInWithPopup method
+    4. Handle the response from the signInWithPopup method
 */
--- src/firebase/firebase.init.js
+++ src/firebase/firebase.init.js
@@ -1,26 +1,23 @@
-// do not store config in client side
-
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+
+// DANGER: DO NOT SHARE CONFIG PUBLICLY
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBmcfMRNXFh-rsFbYLloq4qJcHiB04zI4c",
-  authDomain: "simple-firebase-e53ac.firebaseapp.com",
-  projectId: "simple-firebase-e53ac",
-  storageBucket: "simple-firebase-e53ac.firebasestorage.app",
-  messagingSenderId: "1062884964268",
-  appId: "1:1062884964268:web:e4f61128330e8a68cc2c09",
-  measurementId: "G-453X58S4VS"
+    apiKey: "AIzaSyAl-A7_inhW8gKh_FayJotPpR4byf6lDkw",
+    authDomain: "fir-2-78289.firebaseapp.com",
+    projectId: "fir-2-78289",
+    storageBucket: "fir-2-78289.firebasestorage.app",
+    messagingSenderId: "688496425424",
+    appId: "1:688496425424:web:ae958df5cb1715b2e4a1d5",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 // Initialize Firebase Authentication and get a reference to the service
-const auth = getAuth(app);
-
-export default auth;
+export const auth = getAuth(app);
